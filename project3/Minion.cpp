1ef380287d85786be9d814ece5ba784609962ed6
Kyle Yuen <kyleyuen2506@gmail.com>
2025-04-18 23:50:07 -0700

    final

--- project3/Minion.cpp
+++ project3/Minion.cpp
@@ -14,22 +14,27 @@ Minion::Minion(string n, float h, int e, int b) {
     minionCount++;  // increase count every time a Minion is created
 }
 
+// Returns number of bananas
 int Minion::getBananas() const {
     return bananasOwned;
 }
 
+// Returns name
 string Minion::getName() const{
     return name;
 }
 
+// Prints the number of Minion objects created
 void Minion::printMinionCount() {
     cout << minionCount << " minions created." << endl;
   }
 
+// Compares height (>)
 bool Minion::operator>(const Minion& other) const {
     return this->height>other.height;
 }
 
+// Compares height (<)
 bool Minion::operator<(const Minion& other) const{
     return this->height<other.height;
 }
--- project3/Minion.h
+++ project3/Minion.h
@@ -14,6 +14,7 @@ class Minion{
         static int minionCount;
 
     public:
+        // Constructor: Initializes name, height, eyes, and bananas; increments minionCount
         Minion(string n, float h, int e, int b);
 
         //getters
@@ -23,12 +24,17 @@ class Minion{
         // Static function
         static void printMinionCount();
 
+        // Overloads the > operator to compare minion height
         bool operator>(const Minion& other) const;
+        // Overloads the < operator to compare minion height
         bool operator<(const Minion& other) const;
 
+        // Pre-increment: Adds 1 banana and returns updated object
         Minion& operator++();
+        // Post-increment: Returns copy of object before adding 1 banana
         Minion operator++(int);
-
+        
+        // Friend function to overload the << operator for output
         friend ostream& operator<<(ostream& out, const Minion& m);
 
 };
