673670cad8c3db85d7afad82964a8b305f4d5105
Kyle Yuen <kyleyuen2506@gmail.com>
2025-05-05 00:07:21 -0700

    project 4

---
+++ Project4/main4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello world"<<endl;
+    cout<<"What is your age? ";
+    int age =0;
+    cin>>age;
+    cout<<"Your age is: "<<age;
+}
