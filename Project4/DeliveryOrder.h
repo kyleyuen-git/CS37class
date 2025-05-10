867358989d01b63bc06acf544a54c9f95bcbe04e
Kyle <kyleyuen2506@gmail.com>
2025-05-10 02:57:37 -0700

    DeliveryOrder.h

---
+++ Project4/DeliveryOrder.h
@@ -0,0 +1,16 @@
+#ifndef DELIVERYORDER
+#define DELIVERYORDER
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class DeliveryOrder{
+    private:
+    string name;
+    int date;
+    
+
+};
+
+#endif
